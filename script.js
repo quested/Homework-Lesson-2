6afda7966cbdfebbebcc6204959f4d50eaa46e10
Edik Deineka <e.v.deineka@gmail.com>
2020-02-10 21:58:24 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,115 @@
+var task1 = 'Задача 4. Конкатенация строк';
+console.log(task1);
+
+var first = 255;
+var second = 255;
+var third = 255;
+var fourth = Math.random();
+
+console.log('background-color: rgba(' + first + ', ' + second + ', ' + third + ', ' + fourth + ');');
+
+
+
+var task1 = 'Задача 1';
+console.log(task1);
+
+var age = '1';
+var MIN_AGE = 18;
+
+if (age === '') {
+   let age = Number(age);
+} else if (age >= MIN_AGE) {
+   console.log('Доступ разрешен');
+} else if (age < MIN_AGE) {
+   console.log('Доступ запрещен');
+} else if (age != NaN) {
+   console.log('Вы ввели некоректные данные');
+} else {
+   console.log('Данный формат данных не поддерживается');
+} 
+
+
+
+var task2 = 'Задача 2';
+console.log(task2);
+
+var num1 = 11;
+var num2 = 9;
+var max = num1 >= num2
+? console.log(num1)
+: console.log(num2);
+
+
+
+var task3 = 'Задача 3 (switch case)';
+console.log(task3);
+var yourAge = 12;
+
+switch (true) {
+   case yourAge >= 0 && yourAge < 2:
+      console.log('Сутки только начались');
+      break;
+
+   case yourAge >= 2 && yourAge < 5:
+      console.log('Еще рано, можно спать');
+      break;
+
+   case yourAge >= 5 && yourAge < 7:
+      console.log('Мам, можно еще чуть-чуть по спать');
+      break;
+
+   case yourAge >= 7 && yourAge < 10:
+      console.log('Собираемся в школу');
+      break;
+
+   case yourAge >= 10 && yourAge < 14:
+      console.log('Уроки в школе');
+      break;
+
+   case yourAge >= 14 && yourAge < 17:
+      console.log('Продленка');
+      break;
+
+   case yourAge >= 17 && yourAge < 19:
+      console.log('Дорога домой');
+      break;
+
+   case yourAge >= 19 && yourAge < 23:
+      console.log('Урррааа :)');
+      break;
+
+   case yourAge < 0 || yourAge > 23:
+      console.log('Введите возраст от 0 до 23 лет');
+      break;
+
+   default:
+      console.log('Введите верные данные');
+}
+
+
+
+var task3_2 = 'Задача 3 (if else)';
+console.log(task3_2);
+var yourAge2 = 21;
+
+if (yourAge2 >= 0 && yourAge2 < 2) {
+   console.log('Сутки только начались');
+} else if (yourAge2 >= 2 && yourAge2 < 5) {
+   console.log('Еще рано, можно спать');
+} else if (yourAge2 >= 5 && yourAge2 < 7) {
+   console.log('Мам, можно еще чуть-чуть по спать');
+} else if (yourAge2 >= 7 && yourAge2 < 10) {
+   console.log('Собираемся в школу');
+} else if (yourAge2 >= 10 && yourAge2 < 14) {
+   console.log('Уроки в школе');
+} else if (yourAge2 >= 14 && yourAge2 < 17) {
+   console.log('Продленка');
+} else if (yourAge2 >= 17 && yourAge2 < 19) {
+   console.log('Дорога домой');
+} else if (yourAge2 >= 19 && yourAge2 < 23) {
+   console.log('Урррааа :)');
+} else if (yourAge2 < 0 || yourAge2 > 23) {
+   console.log('Введите возраст от 0 до 23 лет');
+} else {
+   console.log('Введите верные данные');
+}
